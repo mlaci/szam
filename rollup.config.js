b7d0bef582ac1f03c66271995c409c7b8daaea2b
mlaci <9078618+mlaci@users.noreply.github.com>
2022-06-21 13:28:12 +0000

    Restructur

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
-export default {
-    input: 'worker.ts',
+export default [
+  {
+    input: 'src/worker.ts',
     output: {
-      file: 'worker-bundle.js',
+      file: 'docs/worker-bundle.js',
       format: 'esm',
       sourcemap: true
     },
     plugins: [resolve(), typescript()]
-  };
+  },
+  {
+    input: 'src/main.ts',
+    output: {
+      file: 'docs/main-bundle.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    plugins: [resolve(), typescript()]
+  }
+]
